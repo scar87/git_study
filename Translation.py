ddb14c5bd392303410b865ebcc04482ac78f146d
scar87 <rainbowsix.doyun@gmail.com>
2023-01-01 03:19:15 +0900

    translation

---
+++ Translation.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('d:/ws/img/images/60e04a043690eb3567ad4a6e.jpg')
+# 이미지 이동
+row, col = img.shape[:2]
+translation = np.float32([[1,0,40],[0,1,70]])
+img_translation=cv2.warpAffine(img,translation, (row,col)) #warpAffine:이동할 매트릭스(translation) 적용
+cv2.imshow('Translation', img_translation)
+cv2.waitKey()
+cv2.destroyAllWindows
