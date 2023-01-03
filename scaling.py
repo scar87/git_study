f894857be7b0b64d325a9246b5290f5d154d7698
scar87 <rainbowsix.doyun@gmail.com>
2023-01-04 02:11:26 +0900

    scaling

--- scaling.py
+++ scaling.py
@@ -1,7 +1,20 @@
 import cv2
+img = cv2.imread('d:/ws/img/images/60e04a043690eb3567ad4a6e.jpg')
 
 #LinearInterpolation
 img_scale =cv2.resize(img, None, fx=1.3, fy=1.3, interpolation= cv2.INTER_LINEAR)
-cv2.imshow('Liner Interpolation', img_scale)
+cv2.imshow('Linar Interpolation', img_scale)
+cv2.waitKey()
+cv2.destroyAllWindows
+
+#Cubic Interpolation
+img_scale = cv2.resize(img, None, fx=1.3,fy=1.3, interpolation=cv2.INTER_CUBIC)
+cv2.imshow('CUBIC Interpolation', img_scale)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+#Resize
+img_scale = cv2.resize(img, (300,200), interpolation= cv2.INTER_AREA)
+cv2.imshow('Scaling Size', img_scale)
 cv2.waitKey()
 cv2.destroyAllWindows
