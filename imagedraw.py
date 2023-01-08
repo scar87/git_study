9b6ecec17f9999c2fc7503e571b71113346ca29b
scar87 <rainbowsix.doyun@gmail.com>
2023-01-08 22:52:22 +0900

    circle

--- imagedraw.py
+++ imagedraw.py
@@ -40,21 +40,45 @@
 #===========================================
 
 
+# import cv2
+# import numpy as np
+
+# img =np.full((500,500,3),255, dtype=np.unit8)
+
+# poly1 =np.array([[50,50],[200,200]]) #직선
+# poly1 =np.array([[[350,50],[250,200],[450,200]]]) #삼각형
+# poly1 =np.array([[[50,300],[300,200],[200,450],[50,450]]]) #사각형
+# poly1 =np.array([[[350,250],[450,350],[300,450],[250,350]]])#오각형
+
+# cv2.polylines(img, [poly1], True, (0,0,225))
+# cv2.polylines(img, [poly1], True, (0,255,0))
+# cv2.polylines(img, [poly1], True, (225,0,0))
+# cv2.polylines(img, [poly1], False, (0,0,0)) #열린형태
+
+# cv2.imshow('Polylines',img)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+
+#===========================================
+
+
 import cv2
 import numpy as np
 
-img =np.full((500,500,3),255, dtype=np.unit8)
+img= np.full((50,450,3), 255, dtype = np.unit8)
 
-poly1 =np.array([[50,50],[200,200]]) #직선
-poly1 =np.array([[[350,50],[250,200],[450,200]]]) #삼각형
-poly1 =np.array([[[50,300],[300,200],[200,450],[50,450]]]) #사각형
-poly1 =np.array([[[350,250],[450,350],[300,450],[250,350]]])#오각형
+cv2.circle(img,(100, 150),50, (0,0,255)) #원
+cv2.circle(img,(200, 150), 75, (0,255,0), 5)
+cv2.circle(img,(100, 150), 100, (0,0,255), -1)
 
-cv2.polylines(img, [poly1], True, (0,0,225))
-cv2.polylines(img, [poly1], True, (0,255,0))
-cv2.polylines(img, [poly1], True, (225,0,0))
-cv2.polylines(img, [poly1], False, (0,0,0)) #열린형태
+cv2.ellipse(img,(100,300),(50,50), 0,0,360,(0,0,255)) #원
+cv2.ellipse(img,(225,300),(50,50),0,0,180,(0,255,0)) #호
+cv2.ellipse(img,(350,300),(50,50),0,0,180,(255,0,0), -1) #호
 
-cv2.imshow('Polylines',img)
+cv2.ellipes(img,(150, 400),(50,30),0,0,360,(0,0,0)) #타원
+cv2.ellipse(img, (300, 400),(30,50),(45,0,360),(0,0,0),-1)
+
+cv2.imshow('Circle',img)
 cv2.waitKey()
 cv2.destroyAllWindows()
