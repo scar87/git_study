70b7e2b53351a2b04f9d15675eec5b75f5ff8dbd
scar87 <rainbowsix.doyun@gmail.com>
2023-01-12 22:18:14 +0900

    11

--- ROI.py
+++ ROI.py
@@ -11,3 +11,18 @@ cv2.rectangle(roi,(0,0), (h-1, w-1), (0,255,0),5)
 cv2.imshow("IMAGE", img)
 cv2.waitKey()
 cv2.destroyAllWindows()
+
+#마우스 이벤트
+
+import cv2
+import numpy as np
+
+img = cv2.imrad('image.jpg')
+cv2.imshow('img', img)
+drag = False # drag 상태
+defalut_x, default_y, w, h = -1, -1, -1, -1 # 좌표
+blue = (255,0,0)
+
+def Mouse(event,x,y, flag, param):
+    global drag, default_x, dfault_y,img # global variance
+    
