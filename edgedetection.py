8125b7a281c732f3e5c22899ce5571404c3c06d2
scar87 <rainbowsix.doyun@gmail.com>
2023-02-04 03:18:26 +0900

    프리윗 필터

--- edgedetection.py
+++ edgedetection.py
@@ -36,3 +36,25 @@ cv2.imshow('edge_y', edge_y)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
+
+#-----------프리윗 필터---------
+import cv2
+import numpy as np
+
+img= cv2.imread('//Users//doyun//ws//img//images//image.jpg')
+
+# 프리윗 kernel 생성
+G_x = np.array ([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+G_y = np.array ([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
+
+# 필터 적용
+edge_x = cv2.filter2D(img, -1, G_x)
+edge_y = cv2.filter2D(img, -1, G_y)
+
+cv2.imshow('Edge_x', edge_x)
+cv2.imshow('Edge_y', edge_y)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+#
