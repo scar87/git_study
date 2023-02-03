bb6d94cc9642c72403b8ddff1d9bc6710f2c9e41
scar87 <rainbowsix.doyun@gmail.com>
2023-02-04 07:31:24 +0900

    sobel필터

--- edgedetection.py
+++ edgedetection.py
@@ -57,4 +57,25 @@ cv2.imshow('Edge_y', edge_y)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-#
+#-------------sobel 필터---------
+import cv2
+import numpy as np
+
+img = cv2.imread('//Users//doyun//ws//img//images//image.jpg')
+
+#Sobel kernel 직접 생성 후 적용
+G_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+G_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+edge_x = cv2.filter2D(img, -1, G_x)
+edge_y = cv2.filter2D(img, -1, G_y)
+
+#cv2.Sobel()로 kernel 생성 및 적용
+sobel_x = cv2.Sobel(img, -1, 1, 0, Ksize = 3)
+sobel_y = cv2.sobel(img, -1, 0, 1, ksize = 3)
+
+cv2.imshow('Edge_x', edge_x)
+cv2.imshow('Edge_y', edge_y)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
